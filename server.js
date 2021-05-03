2ed3b2e2e8b508dec8dd12e43d86fdbed713cf20
hovanbuu <hovanbuu@gmail.com>
2021-05-03 15:12:43 +0700

    _ add http listener _ add barebone web app

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const db = require('./db/db.js');
 const common = require('./common/common.js');
 const rssCrawler = require('./rss/crawler/crawler.js');
 
-start();
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+const systemConfig = require('./systemConfig.js');
+const app = express();
 
+start();
 
 async function start() {
     let prepareDbConnectionResult = await prepareDbConnection();
@@ -11,9 +16,9 @@ async function start() {
         return;
     }
     rssCrawler.start();
+
+    prepareHttpServer();
 };
-
-
 
 async function prepareDbConnection() {
     common.consoleLog('Begin to establish database connection...');
@@ -24,4 +29,14 @@ async function prepareDbConnection() {
     }
     common.consoleLog('Established database connection.');
     return true;
+};
+
+function prepareHttpServer() {
+    app.use(express.json());
+    app.use(express.urlencoded({ limit: '10mb', extended: false, }));
+    app.use(cors());
+    http.createServer(app).listen(systemConfig.httpPort, function () {
+        common.consoleLog('HTTP Server started on port' + systemConfig.httpPort + '.');
+        require('./web/web.js')(app);
+    });
 };
--- systemConfig.js
+++ systemConfig.js
@@ -2,4 +2,5 @@ module.exports = {
     dayjsFormatDateOnly: 'YYYY-MM-DD',
     dayjsFormatFull: 'YYYY-MM-DD HH:mm:ss',
     consoleColor: '\x1b[5m\x1b[43m\x1b[31m',
+    httpPort: 9067,
 };
