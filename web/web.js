5a5d819c4490b6c29b745a9ab60bdb4bfbade9a0
hovanbuu <hovanbuu@gmail.com>
2021-05-03 15:53:02 +0700

    _ remove console log

--- web/web.js
+++ web/web.js
@@ -1,6 +1,5 @@
 module.exports = function (app) {
     app.get('/test.html', function (request, response) {
-        console.log(request.body);
         response.statusCode = 200;
         response.setHeader('Content-Type', 'text/plain');
         response.end('Hello World!\n');
