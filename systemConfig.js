2562a684145c5359e4385b0cca9da4d374aa6526
hovanbuu <hovanbuu@gmail.com>
2021-05-07 13:35:26 +0700

    _ systemConfig.js add vn format full

--- systemConfig.js
+++ systemConfig.js
@@ -1,7 +1,8 @@
 module.exports = {
     dayjsFormatDateOnly: 'YYYY-MM-DD',
     dayjsFormatFull: 'YYYY-MM-DD HH:mm:ss',
-    dayjsVNFormatDateOnly = 'DD-MM-YYYY',
+    dayjsVNFormatDateOnly: 'DD-MM-YYYY',
+    dayjsVNFormatFull: 'MM-DD-YYYY HH:mm:ss',
     consoleColor: '\x1b[5m\x1b[43m\x1b[31m',
     httpPort: 9067,
 
