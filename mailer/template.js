81735cfa728d9783d842c802b0df148eb9c36b5c
hovanbuu <hovanbuu@gmail.com>
2021-05-07 14:25:38 +0700

    _ template.js: format win email template

--- mailer/template.js
+++ mailer/template.js
@@ -16,9 +16,7 @@ module.exports = {
                     <p>Xin chúc mừng quý khách!!!</p>
                     <p>Quý khách đã may mắn mua được vé trúng thưởng của kỳ xổ số 
                     ngày |<|callDate|>|. Chi tiết trúng thưởng của quý khách như sau:</p>
-                    <br/>
                     |<|publisherDetail|>|
-                    <br/>
                     <p>Theo quy định của các công ty xổ số, quý khách có 30 ngày để 
                     đổi vé lấy tiền trúng giải. Xin quý khách vui lòng đổi vé 
                     trúng trước ngày |<|lastClaimDay|>|. Nếu quý khách dự định đổi 
@@ -27,20 +25,18 @@ module.exports = {
                     phát hành (có thể bị từ chối do quá cận ngày và/hay chỉ đổi 
                     vài tờ). Công ty xổ số luôn sẽ trao giải trong thời gian 
                     quy định (trong vòng 30 ngày kể từ ngày xổ).</p>
-                    <br/>
                     <p>Cũng theo quy định, người trúng xổ số sẽ cần phải đóng thuế 
                     thu nhập cá nhân, áp dụng đối với trường hợp  giải thưởng có 
                     giá trị từ 10 triệu đồng trở lên. Với giá trị giải thưởng trên,
                     quý khách sẽ |<|taxSummary|>|</p>
-                    <br/>
                     <p>Một lần nữa xin chức mừng quý khách! Chúc quý khách luôn tìm
-                    được nhiều niềm vui trong cuộc sống!</p>
-                    <p>Trân trọng,</p>
-                    <br/>
-                    <p>Quý khách nhận được thư này vì đã sử dụng dịch vụ báo vé trúng
+                    được nhiều niềm vui trong cuộc sống!<br>
+                    Trân trọng,</p>
+                    <p style='font-style: italic; color: gray; font-size:8px;'>
+                    Quý khách nhận được thư này vì đã sử dụng dịch vụ báo vé trúng
                     thưởng tại baotrungso.com. Nếu không phải là người nhận mong 
                     muốn của thư này, mong quý bạn vui lòng bỏ quả sự cố này!</p>
-                <div>
+                </div>
             </body>
         </html>
         `,
