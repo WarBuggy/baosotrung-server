890ae880e3809c1b9a8bde69b9a20430ea4f48ff
hovanbuu <hovanbuu@gmail.com>
2021-05-11 15:55:58 +0700

    _ systemConfig.js: really remove (correctly) the blink effect out of console color

--- systemConfig.js
+++ systemConfig.js
@@ -3,7 +3,7 @@ module.exports = {
     dayjsFormatFull: 'YYYY-MM-DD HH:mm:ss',
     dayjsVNFormatDateOnly: 'DD-MM-YYYY',
     dayjsVNFormatFull: 'MM-DD-YYYY HH:mm:ss',
-    consoleColor: '\x1b[5m\x1b[43m',
+    consoleColor: '\x1b[43m\x1b[31m',
     httpPort: 9067,
 
     prizeMoneyTaxThreshold: 10000000,
