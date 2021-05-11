9240c952430e2318c7e516c36ebd3a1298c5379b
hovanbuu <hovanbuu@gmail.com>
2021-05-11 15:21:00 +0700

    _ index.js: remove debug log

--- public/script/index.js
+++ public/script/index.js
@@ -1,6 +1,5 @@
 window.onload = async function () {
-    let coreData = await Common.sendToBackend('/api/data/core');
-    console.log(coreData);
+    window.coreData = await Common.sendToBackend('/api/data/core');
     window.inputSeriesGrid = new InputSeriesGrid();
 };
 
