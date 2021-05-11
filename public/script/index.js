17345c290cba2f70308c6a1bfe7a22ba598d27ee
hovanbuu <hovanbuu@gmail.com>
2021-05-11 15:22:06 +0700

    _ index.js: window.coreData only contains the useful part

--- public/script/index.js
+++ public/script/index.js
@@ -1,5 +1,5 @@
 window.onload = async function () {
-    window.coreData = await Common.sendToBackend('/api/data/core');
+    window.coreData = await Common.sendToBackend('/api/data/core').data;
     window.inputSeriesGrid = new InputSeriesGrid();
 };
 
